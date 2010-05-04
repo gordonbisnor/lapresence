321b55f830bfb58b1e6f442d854e65db2587c842
Gordon B. Isnor <gordon@gordon-b-isnors-macbook-pro.local>
2010-05-04 13:57:39 -0400

    tiny mce problems

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,17 +3,19 @@ password = "a37df06c"
 application = "lapresence"
 domain = 'lapresence.webfactional.com'
 
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+set :keep_releases, 4 
 set :rake, "/home/#{user}/webapps/#{application}/bin/rake"
 set :webfaction_username, "#{user}"
 set :user, user
 set :domain, domain
-set :application, "point"
+set :application, "#{application}"
 set :repository,  "#{user}@#{domain}:/home/#{user}/git/#{application}"
 set :scm, :git
 set :deploy_via, :checkout 
-set :scm_command, "/home/#{user}/git/bin/git"
+set :scm_command, "/home/#{user}/bin/git"
 set :local_scm_command, "git"
 set :scm_username, user
 set :scm_password, "#{password}"
@@ -36,3 +38,11 @@ namespace :deploy do
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
  end
 end
+
+after "deploy:update", "deploy:cleanup" # OTHER WISE OLD RELEASES WILL NOT BE DELETED EVEN THOUGH KEEP IS SET
+
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'
--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'formtastic'
-  config.gem 'tiny_mce'
+  config.gem 'tiny_mce', :lib => 'tiny_mce', :source => 'gems.github.com'
+  config.gem 'hoptoad_notifier'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

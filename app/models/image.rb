1180cd01af4841ae632209570a612e8005eef81c
Gordon B. Isnor <gordon@gbi.local>
2010-05-15 14:03:55 -0400

    image should not show the cache param

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   has_attached_file :attachment, :styles => { :thumb => "100x100#" }
 
   def url
-    APP_CONFIG[:site_url] + attachment.url
+    APP_CONFIG[:site_url] + attachment.url.split("?")[0]
   end
   
 end

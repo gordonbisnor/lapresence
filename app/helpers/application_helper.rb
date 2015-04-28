3acb100476f6a6ac018eb5a95dc9be97b5e401ce
Gordon B. Isnor <gordonbisnor@gmail.com>
2015-04-27 21:06:49 -0300

    oops

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,31 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
     
+  ALERT_TYPES_MAP = {
+    notice: :success,
+    alert: :danger,
+    error: :danger,
+    info: :info,
+    warning: :warning
+  }
+
+  def bootstrap_flash
+    safe_join(flash.each_with_object([]) do |(type, message), messages|
+                next if message.blank? || !message.respond_to?(:to_str)
+                type = ALERT_TYPES_MAP.fetch(type.to_sym, type)
+                messages << flash_container(type, message)
+              end, "\n").presence
+  end
+
+  def flash_container(type, message)
+    content_tag :div, class: "alert alert-#{type} alert-dismissable" do
+      button_tag type: "button", class: "close", data: { dismiss: "alert" } do
+        "&times;".html_safe
+      end.safe_concat(message)
+    end
+  end
+end
+
   def dates groupe
      if groupe.start_date.present? && groupe.end_date.present?
      "#{l(groupe.start_date, :format => :long_ordinal)} - #{l(groupe.end_date, :format => :long_ordinal)}"

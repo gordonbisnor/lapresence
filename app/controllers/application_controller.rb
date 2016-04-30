5eaa979be7cb81e2443cdd42d2c01f5b53314128
Gordon B. Isnor <gordonbisnor@gmail.com>
2016-04-30 07:46:42 -0300

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,5 +23,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     admin_dashboard_url
   end
+
+  def after_sign_out_path_for(resource)
+    bio_path
+  end
     
 end

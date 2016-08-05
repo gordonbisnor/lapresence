78c7ee545ab1fa5f47bbeb60b7c5d164aa18973b
Gordon B. Isnor <gordonbisnor@gmail.com>
2016-08-05 13:49:53 -0300

    disable airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,3 @@
-Airbrake.configure do |config|
-  config.api_key = '5a1aef49322c0273331b1d2288ae4ed6'
-end
+#Airbrake.configure do |config|
+#  config.api_key = '5a1aef49322c0273331b1d2288ae4ed6'
+#end

26a2c95c571c9e5e1879b48f4b2e80cee48340d5
Gordon B. Isnor <gordonbisnor@gmail.com>
2019-07-02 16:46:02 -0300

    versions controller bug fix

--- app/controllers/admin/versions_controller.rb
+++ app/controllers/admin/versions_controller.rb
@@ -11,10 +11,19 @@ class Admin::VersionsController < Admin::AdminController
     else
       @version.item.destroy
     end
-    
-    link_name = params[:redo] == "true" ? t('undo') : t('redo')
-    link = view_context.link_to(link_name, admin_revert_version_path(@version.next, redo: !params[:redo]),method: :post, class: "btn btn-xs pull-right btn-success")
-    redirect_to :back, notice: "Undid #{@version.event}. #{link}".html_safe
+
+    flash[:notice] = "Undid #{@version.event}. #{link}".html_safe
+    redirect_back(fallback_location: admin_dashboard_path)
   end
+
+  def link_name
+    params[:redo] == "true" ? t('undo') : t('redo')
+  end
+  private :link_name
+
+  def link
+    view_context.link_to(link_name, admin_revert_version_path(@version.next, redo: !params[:redo]),method: :post, class: "btn btn-xs pull-right btn-success")
+  end
+  private :link
   
 end

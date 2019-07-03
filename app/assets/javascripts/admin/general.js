e5452e9aea6678dfe15b3a15d44b4cb63bc2c1fc
Gordon B. Isnor <gordonbisnor@gmail.com>
2019-07-03 13:33:34 -0300

    tiny pro  eval

--- app/assets/javascripts/admin/general.js
+++ app/assets/javascripts/admin/general.js
@@ -2,7 +2,16 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function(){
-  $('.tinymce').tinymce();
+
+  tinymce.init({
+    selector: '.tinymce',
+    plugins: 'a11ychecker advcode casechange formatpainter linkchecker lists checklist media mediaembed pageembed permanentpen powerpaste tinycomments tinydrive tinymcespellchecker',
+    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter  pageembed permanentpen',
+    toolbar_drawer: 'floating',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name'
+ });
+
 	$('.close').click(function () {
 		$(this).parent('div').fadeOut(1000);
 	});

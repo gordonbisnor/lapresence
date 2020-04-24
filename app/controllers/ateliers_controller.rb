dab04a44398936155d7d590b0a5a7cabc3cf5d2e
Gordon B. Isnor <gordonbisnor@gmail.com>
2020-04-24 12:28:46 -0300

    seo

--- app/controllers/ateliers_controller.rb
+++ app/controllers/ateliers_controller.rb
@@ -2,11 +2,13 @@ class AteliersController < ApplicationController
   before_action :french
   
   def index
+    @seo_description = "Horaire Des Ateliers par Lorraine Desmarais"
     @items = Atelier.published.all
   end
 
   def show
     @item = Atelier.published.find(params[:id])
+    @page_title = @item.title
   end
 
 end

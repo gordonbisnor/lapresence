fd9cef38853cf3b9f2892eb1a7a3d053e7e3a9a4
Gordon B. Isnor <gordonbisnor@gmail.com>
2020-11-27 17:11:19 -0400

    deploy debug for webpack

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,3 +92,16 @@ set :disallow_pushing, false
 
 # if you prefer bzip2/unbzip2 instead of gzip
 set :compressor, :bzip2
+
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end

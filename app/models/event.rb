92716ae9c0ad01c76f185a5a3c21305c9f19a503
Gordon B. Isnor <gordonbisnor@gmail.com>
2020-11-27 09:54:10 -0400

    make event pictures use amazon

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,22 @@ class Event < ApplicationRecord
   has_attached_file :picture,
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename",
-    :styles => { :thumb => "200x1000>" }
-  
+    :styles => { :thumb => "200x1000>" },
+    :storage => :s3,
+    :s3_credentials => Proc.new { |a| a.instance.s3_credentials },
+    :s3_permissions => {
+      :original => "public-read"
+    },
+    s3_protocol: "https",
+    bucket: ENV['S3_BUCKET'],
+    s3_region: "ca-central-1",
+    path: "/:attachment/:id/:style/:filename",
+    url: ":s3_domain_url"
+
+  def s3_credentials
+    {:bucket => ENV['S3_BUCKET'], :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] }
+  end
+
   do_not_validate_attachment_file_type :picture
 
   def dates 

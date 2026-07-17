c0e9956e5c7d61e1653d975308e55a31bdd55913
Gordon Isnor <gordonbisnor@gmail.com>
2026-07-17 02:35:16 -0300

    Fix webpack 4 hashing on modern Node

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+// Prevent OpenSSL 3 errors on modern Node by avoiding md4
+environment.config.merge({
+  output: {
+    hashFunction: 'sha256',
+  },
+})
+
 module.exports = environment

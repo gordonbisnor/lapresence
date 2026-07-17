5d5ce5eb67d1f0881df1c5f01b0fc4d749bf6ef1
Gordon Isnor <gordonbisnor@gmail.com>
2026-07-17 02:04:12 -0300

    deploy debugt

--- config/deploy.rb
+++ config/deploy.rb
@@ -100,7 +100,7 @@ before 'deploy:assets:precompile', 'deploy:yarn_install'
 namespace :deploy do
   desc 'Run rake yarn install'
   task :yarn_install do
-    on roles(:web) do
+    on roles(:all) do
       within release_path do
         #execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
         #
